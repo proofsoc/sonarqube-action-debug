2b141baa3588869d4d460b2124c27fe18cf4063e
fabioportes <fabioportes@users.noreply.github.com>
2023-08-21 19:03:37 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,6 @@ if [[ ! -f "${INPUT_PROJECTBASEDIR%/}sonar-project.properties" ]]; then
     -Dsonar.sources="${INPUT_PROJECTBASEDIR}" \
     -Dsonar.sourceEncoding="${INPUT_ENCODING}" \
     -Dsonar.token="${INPUT_LOGIN}" \
-    -Dsonar.scm.provider="git" \
     -X
 else
   sonar-scanner \
@@ -46,6 +45,5 @@ else
     #-Dsonar.login="${INPUT_LOGIN}" \
     #-Dsonar.password="${SONAR_PASSWORD}" \
     -Dsonar.token="${INPUT_LOGIN}" \
-    -Dsonar.scm.provider="git" \
     -X
 fi
